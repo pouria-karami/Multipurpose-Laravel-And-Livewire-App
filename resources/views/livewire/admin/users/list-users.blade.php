247deaafd0aa95327ef523e879212da34ff767df
Pouria <pouria_karami@yahoo.com>
2022-04-01 18:28:59 +0430

    Working on multipurpose laravel and livewire

---
+++ resources/views/livewire/admin/users/list-users.blade.php
@@ -0,0 +1,175 @@
+<div xmlns:wire="http://www.w3.org/1999/xhtml">
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark">Users</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                        <li class="breadcrumb-item active">Users</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="d-flex justify-content-start mb-2">
+                        <button wire:click.prevent="addNew" class="btn btn-primary"><i
+                                class="fa fa-plus-circle mr-1"></i> Add New User
+                        </button>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <table class="table table-striped table-hover">
+                                <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Options</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach ($users as $user)
+                                    <tr>
+                                        <th scope="row">{{ $loop->iteration }}</th>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{$user->email}}</td>
+                                        <td>
+                                            <a href="" wire:click.prevent="edit({{ $user }})">
+                                                <i class="fa fa-edit mr-2"></i>
+                                            </a>
+                                            <a href="" wire:click.prevent="confirmUserRemoval({{ $user->id }})">
+                                                <i class="fa fa-trash text-danger "></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+
+    <!-- Modal -->
+    <div class="modal fade" id="form" tabindex="-1"
+         aria-labelledby="exampleModalLabel" aria-hidden="true"
+         wire:ignore.self>
+        <div class="modal-dialog">
+            <form autocomplete="off"
+                  wire:submit.prevent="{{ $showEditModal ? 'updateUser' : 'createUser' }}">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">
+                            @if ($showEditModal)
+                                <span>Edit User</span>
+                            @else
+                                <span>Add New User</span>
+                            @endif
+                        </h5>
+                        <button type="button" class="close"
+                                data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" wire:model.defer="state.name"
+                                   class="form-control @error('name') is-invalid @enderror"
+                                   id="name" aria-describedby="nameHelp" placeholder="Enter Full Name">
+                            @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email address</label>
+                            <input type="text" wire:model.defer="state.email"
+                                   class="form-control @error('email') is-invalid @enderror " id="email"
+                                   aria-describedby="emailHelp" placeholder="Enter Email">
+                            @error('email')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" wire:model.defer="state.password"
+                                   class="form-control @error('password') is-invalid @enderror " id="password"
+                                   placeholder="Enter Password">
+                            @error('password')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="passwordConfirmation" class="form-label">Confirm Password</label>
+                            <input type="password" wire:model.defer="state.password_confirmation"
+                                   class="form-control"
+                                   id="passwordConfirmation"
+                                   placeholder="Confirm Password">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal"><i
+                                class="fa fa-times mr-1"></i>Cancel
+                        </button>
+                        <button type="submit" class="btn btn-primary">
+                            @if ($showEditModal)
+                                <span><i class="fa fa-user-edit mr-1"></i>Save Change</span>
+                            @else
+                                <span><i class="fa fa-save mr-1"></i>Save</span>
+                            @endif
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!--/ Modal -->
+
+    <!--DeleteModal -->
+    <div class="modal fade" id="confirmationModal" tabindex="-1"
+         aria-labelledby="exampleModalLabel" aria-hidden="true"
+         wire:ignore.self>
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">
+                        Delete User
+                    </h5>
+                </div>
+                <div class="modal-body">
+                    <h4>Are You Sure You Want To Delete This User? </h4>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                        <i class="fa fa-times mr-1"></i>Cancel
+                    </button>
+                    <button type="button" wire:click.prevent="deleteUser" class="btn btn-danger"><span class="fa fa-trash"></span> Delete User
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ DeleteModal -->
+</div>
